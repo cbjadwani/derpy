cdaaaf1d9131b72cd257108f5d97dc0f3c889af2
Chirag Jadwani <chirag.jadwani@gmail.com>
2012-09-04 18:18:09 +0530

    Removed bugs and improved compaction

--- derpy.py
+++ derpy.py
@@ -1,6 +1,3 @@
-from utils import memoize
-
-
 class Token(object):
     def __init__(self, token):
         self.token = token
@@ -8,78 +5,127 @@ class Token(object):
     def get_token(self):
         return self
 
+    @property
+    def nullable(self):
+        return self == EPS
+
+    def derive(self, token):
+        if self == token:
+            return EPS
+        return NUL
+
     def __eq__(self, other):
-        if isinstance(other, Language):
+        if isinstance(other, Grammar):
             return other.get_token() == self
-        return other is not None and self.token == other.token
+        return isinstance(other, Token) and self.token == other.token
+
+    def __hash__(self):
+        return hash(self.token)
+
+    def __and__(self, other):
+        x = self
+        y = other.get_token() or other
+        if x == NUL or y == NUL:
+            return NUL
+        if x == EPS:
+            return y
+        if y == EPS:
+            return x
+        return Grammar(self, CAT, other)
+
+    def __or__(self, other):
+        x = self
+        y = other.get_token() or other
+        if x == y:
+            return x
+        if x == NUL:
+            return y
+        if y == NUL:
+            return x
+        return Grammar(self, ALT, other)
 
     def __repr__(self):
         return 'Token(%r)' % self.token
 
-
-EPS = Token('')         # epsilon, 'empty' token
-NUL = Token(None)       # Null, no match token
-
-
-class Op(object):
-    def __init__(self, opname):
-        self.opname = opname
-
-    def __repr__(self):
-        return 'Op(%r)' % self.opname
+    def graph(self, graphed):
+        gid = graph_id(self)
+        if gid in graphed:
+            return
+        print
+        print '  %s [label="%s" shape=box]' % (gid, str(self.token))
+        graphed[gid] = True
 
 
-CAT = Op('CAT')
-ALT = Op('ALT')
+EPS = Token(True)       # epsilon or 'empty' token
+NUL = Token(None)       # Null or 'no match' token
+LAZY_TOKEN = Token(Ellipsis)
+CAT = 'CAT'
+ALT = 'ALT'
 
 
-class Language(object):
-    def __init__(self, language1, operation=None, language2=None):
-        self.language1 = language1.get_token() or language1
+class Grammar(object):
+    def __init__(self, lgrammar, operation=None, rgrammar=None):
+        self.lgrammar = lgrammar.get_token() or lgrammar
         self.operation = operation
-        self.language2 = operation and language2.get_token() or language2
-        self._nullable = None
+        self.rgrammar = operation and rgrammar.get_token() or rgrammar
         self._derivations = {}
+        self._nullable = None
 
     def get_token(self):
-        if self.operation is None and isinstance(self.language1, Token):
-            return self.language1
+        if self.operation is None and isinstance(self.lgrammar, Token):
+            return self.lgrammar
         return None
 
-    def __add__(self, other):
-        other_token = other.get_token()
-        self_token = self.get_token()
-        if other_token == NUL or self_token == NUL:
-            return Language(NUL)
-        if other_token == EPS:
-            return self
-        if self_token == EPS:
-            return Language(other_token) if other_token else other
-        return Language(self, CAT, other)
+    def _fold(self):
+        assert self.operation is CAT
+        l = self.lgrammar
+        r = self.rgrammar
+        seen = [self]
+        while isinstance(l, Grammar):
+            if l.operation is None:
+                l = l.lgrammar
+                self.lgrammar = l
+                continue
+            if l.operation is not CAT:
+                break
+            if l in seen:
+                # Infinite left recursion, can not match anything
+                print ' .==.'
+                self.lgrammar = NUL
+                self.operation = None
+                self.rgrammar = None
+                return
+            seen.append(l)
+            l, r = l.lgrammar, Grammar(l.rgrammar, CAT, r)
+        self.lgrammar = l
+        self.rgrammar = r
+
+    def __and__(self, other):
+        x = self.get_token() or self
+        y = other.get_token() or other
+        if x == NUL or y == NUL:
+            return NUL
+        if x == EPS:
+            return y
+        if y == EPS:
+            return x
+        res = Grammar(x, CAT, y)
+        res._fold()
+        r = res.rgrammar
+        if isinstance(r, Grammar) and r.operation is CAT:
+            r._fold()
+        return res
 
     def __or__(self, other):
-        other_token = other.get_token()
-        self_token = self.get_token()
-        if other_token == NUL:
-            return self
-        if self_token == NUL:
-            return Language(other_token) if other_token else other
-        if self is other:
-            return self
-        return Language(self, ALT, other)
-
-    def starplus(self):
-        eps = Language(EPS)
-        plus = Language(self, CAT, eps)
-        star = Language(eps, ALT, plus)
-        plus.language2 = star
-        return (star, plus)
-
-    def star(self):
-        return self.starplus()[0]
-
-    def plus(self):
-        return self.starplus()[1]
+        x = self.get_token() or self
+        y = other.get_token() or other
+        if x == NUL:
+            return y
+        if y == NUL:
+            return x
+        if x is y:
+            return x
+        return Grammar(x, ALT, y)
 
     @property
     def nullable(self):
@@ -88,64 +134,61 @@ class Language(object):
         return self._nullable
 
     def _is_nullable(self):
-        self._nullable = False
+        self._nullable = False  # Initail value to break recursions
+        l = self.lgrammar
+        r = self.rgrammar
         if self.operation is None:
-            self_token = self.get_token()
-            if self_token == EPS:
-                return True
-            elif self_token:
-                return False
-            else:
-                return self.language1.nullable
-        l1 = self.language1
-        l2 = self.language2
-        if isinstance(l1, Token):
-            l1 = Language(l1)
-        if isinstance(l2, Token):
-            l2 = Language(l2)
+            return self.lgrammar.nullable
         if self.operation is CAT:
-            return l1.nullable and l2.nullable
+            return l.nullable and r.nullable
         if self.operation is ALT:
-            return l1.nullable or l2.nullable
+            return l.nullable or r.nullable
         assert False, "Bad! %s" % self.operation
 
     def derive(self, token):
-        d = self._derivations.get(token)
-        if d is not None: return d
-        self._derivations[token] = d = Language(NUL)
-        if self.operation is None:
-            self_token = self.get_token()
-            if self_token == token:
-                d.language1 = EPS
-                return d
-            elif self_token:
-                return d
-            else:
-                self._derivations[token] = self.language1.derive(token)
-                return self._derivations[token]
-        l1 = self.language1
-        l2 = self.language2
-        if isinstance(l1, Token):
-            l1 = Language(l1)
-        if isinstance(l2, Token):
-            l2 = Language(l2)
-        if self.operation is CAT:
-            d1 = l1.derive(token) + l2
-            if not l1.nullable:
-                self._derivations[token] = d1
-                return self._derivations[token]
-            self._derivations[token] = d1 | l2.derive(token)
+        try:
             return self._derivations[token]
-        if self.operation is ALT:
-            d1 = l1.derive(token)
-            d2 = l2.derive(token)
-            self._derivations[token] = d1 | d2
-            return self._derivations[token]
-        assert False, "Bad! %s" % self.operation
+        except KeyError:
+            pass
+        cached = self._derivations[token] = Grammar(LAZY_TOKEN, ALT, EPS)
+        res = self._derive(token)
+        assert res is not cached
+        if not isinstance(res, Grammar) or res.is_lazy():
+            cached.lgrammar = res
+            cached.operation = None
+            cached.rgrammar = None
+            if isinstance(res, Grammar) and res.is_lazy():
+                print ' --x--', graph_id(res), graph_id(cached)
+        else:
+            cached.lgrammar = res.lgrammar
+            cached.operation = res.operation
+            cached.rgrammar = res.rgrammar
+            if cached.operation is CAT:
+                cached._fold()
+        return cached
 
-    def match(self, raw_tokens):
-        tokens = map(Token, raw_tokens)
-        return self.match_(tokens)
+    def _derive(self, token):
+        if self.lgrammar == LAZY_TOKEN:
+            raise Exception('Fake!')
+        l = self.lgrammar
+        r = self.rgrammar
+        res = None
+        if self.operation is None:
+            res = l.derive(token)
+        elif self.operation is CAT:
+            d1 = l.derive(token) & r
+            if not l.nullable:
+                res = d1
+            else:
+                res = d1 | r.derive(token)
+        elif self.operation is ALT:
+            d1 = l.derive(token)
+            d2 = r.derive(token)
+            res = d1 | d2
+        else:
+            assert False, "Bad! %s" % self.operation
+        assert res is not None
+        return res
 
     def match_(self, tokens):
         l = self
@@ -155,10 +198,124 @@ class Language(object):
                 return False
         return l.nullable
 
+    def match(self, raw_tokens):
+        tokens = map(Token, raw_tokens)
+        return self.match_(tokens)
+
+    def graph(self, graphed=None):
+        firstcall = graphed is None
+        gid = graph_id(self)
+        if firstcall:
+            graphed = {}
+            print 'digraph derpy {'
+            print 'graph [layout=dot rankdir=TB]'
+        elif gid in graphed:
+            return
+        l, r = self.lgrammar, self.rgrammar
+        lid, rid = graph_id(l), graph_id(r)
+        print
+        print '  %s[label="%s"%s]' % (gid, self.operation or 'S', ' shape=invhouse' if firstcall else '')
+        print '    %s -> %s[label="L"%s]' % (gid, lid, ' constraint=false' if lid in graphed else '')
+        if self.operation is not None:
+            print '    %s -> %s[label="R"%s]' % (gid, rid, ' constraint=false' if rid in graphed else '')
+        graphed[gid] = True
+        l.graph(graphed)
+        if self.operation is not None:
+            r.graph(graphed)
+        if firstcall:
+            print '}'
+        return graphed
+
+    def __iter__(self):
+        return self._iterate()
+
+    def _iterate(self, iterated=None):
+        l = self.lgrammar
+        r = self.rgrammar
+        sid = id(self)
+        if iterated is None:
+            iterated = {}
+        else:
+            if sid in iterated:
+                return
+        iterated[sid] = True
+        assert l
+        if isinstance(l, Grammar):
+            for i in l._iterate(iterated):
+                yield i
+        else:
+            yield l
+        yield self
+        if isinstance(r, Grammar):
+            for i in r._iterate(iterated):
+                yield i
+        elif r is not None:
+            yield r
+
+    @classmethod
+    def create_lazy(cls):
+        return cls(LAZY_TOKEN, ALT, EPS)
+
+    def is_lazy(self):
+        if self.lgrammar == LAZY_TOKEN:
+            if self.operation is ALT and self.rgrammar == EPS:
+                return True
+            raise Exception("Fake lazy!")
+        return False
+
+    def any_lazy(self, checked=None):
+        for i in self:
+            if isinstance(i, Grammar) and i.is_lazy():
+                return True
+        return False
+
+    def derive_many(self, tokens):
+        d = self
+        for token in tokens:
+            d = d.derive(token)
+            if d is NUL:
+                break
+        return d
+
+    def starplus(self):
+        eps = Grammar(EPS)
+        plus = Grammar(eps, CAT, self)
+        star = Grammar(eps, ALT, plus)
+        plus.lgrammar = star
+        return (star, plus)
+
+    def star(self):
+        return self.starplus()[0]
+
+    def plus(self):
+        return self.starplus()[1]
+
+
+def graph_id(obj):
+    ID = str(abs(id(obj)))
+    c = chr(ord(ID[0]) + ord('A') - ord('0'))
+    return c + ID[1:]
 
 chr_range = lambda a, b: ''.join(map(chr, range(ord(a), ord(b)+1)))
-char = lambda c: Language(Token(c))
-from_string = lambda s: reduce(lambda a, b: a | b, map(char, s))
-alpha = from_string(chr_range('A', 'Z') + chr_range('a', 'z'))
-num = from_string('0123456789')
-word = alpha + (alpha | num).star()
+char = lambda c: Grammar(Token(c))
+anychar = lambda s: reduce(lambda a, b: a | b, map(char, s))
+alpha = anychar(chr_range('A', 'Z') + chr_range('a', 'z'))
+num = anychar('0123456789')
+word = alpha & (alpha | num).star()
+
+a = char('a')
+S = (a & char('(') & a & char(')')) | Grammar(EPS)
+#S.lgrammar.lgrammar.lgrammar.lgrammar = S
+#S.lgrammar.lgrammar.rgrammar = S
+S.lgrammar.rgrammar.rgrammar.lgrammar = S
+S.lgrammar.lgrammar = S
+
+s = (a & char('+') & a) | char('1')
+s.lgrammar.rgrammar.rgrammar = s
+s.lgrammar.lgrammar = s
+#s.lgrammar.lgrammar.lgrammar = s
+#s.lgrammar.rgrammar = s
+"""
+S = (((S o) S) c) | e
+dS = (dS
+"""
--- utils.py
+++ utils.py
@@ -1,6 +1,14 @@
 from functools import partial
 
-class memoize_recursive(object):
+
+def memoize(*args, **kwargs):
+    if not args:
+        return lambda f: _memoize_default(f, **kwargs)
+    function = args[0]
+    return _memoize_default(function, **kwargs)
+
+
+class _memoize_default(object):
     """cache the return value of a method
 
     Original source: code.activestate.com/recipes/577452-a-memoize-decorator-for-instance-methods/
@@ -27,11 +35,23 @@ class memoize_recursive(object):
     def __init__(self, func, **kwargs):
         self.func = func
         try:
-            self.default_value = kwargs.pop('default')
+            self.default_value = kwargs.pop('default_value')
         except KeyError:
-            pass
+            try:
+                self.default_maker = kwargs.pop('default_maker')
+            except KeyError:
+                pass
         if kwargs:
-            raise TypeError('Got unexpected kwargs: %s' % kwargs.keys())
+            raise TypeError('Got unexpected/extra kwargs: %s' % kwargs.keys())
+
+    def _get_default_value(self):
+        '''Try to return the default_value or create one from default_maker
+        Raises AttributeError if no default_value of default_maker is set
+        '''
+        try:
+            return self.default_value
+        except AttributeError:
+            return self.default_maker()
 
     def __get__(self, obj, objtype=None):
         if obj is None:
@@ -48,9 +68,10 @@ class memoize_recursive(object):
         try:
             res = cache[key]
         except KeyError:
-            if hasattr(self, 'default_value'):
-                default_value = self.default_value
-                cache[key] = default_value
+            try:
+                cache[key] = self._get_default_value()
+            except AttributeError:
+                pass
             res = cache[key] = self.func(*args, **kw)
         return res
 
