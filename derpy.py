75eda570c2af8031e35ce50e51e9dba5ebd3e142
Chirag Jadwani <chirag.jadwani@gmail.com>
2012-09-10 00:32:51 +0530

    Lots of changes

--- derpy.py
+++ derpy.py
@@ -5,7 +5,7 @@ class Token(object):
     def __init__(self, token):
         self.token = token
 
-    def get_token(self):
+    def get_value(self):
         return self
 
     @property
@@ -19,7 +19,7 @@ class Token(object):
 
     def __eq__(self, other):
         if isinstance(other, Grammar):
-            return other.get_token() == self
+            other = other.get_value()
         return isinstance(other, Token) and self.token == other.token
 
     def __hash__(self):
@@ -27,25 +27,25 @@ class Token(object):
 
     def __and__(self, other):
         x = self
-        y = other.get_token() or other
+        y = other.get_value()
         if x == NUL or y == NUL:
             return NUL
         if x == EPS:
             return y
         if y == EPS:
             return x
-        return Grammar(self, CAT, other)
+        return Grammar(x, CAT, y)
 
     def __or__(self, other):
         x = self
-        y = other.get_token() or other
+        y = other.get_value()
         if x == y:
             return x
         if x == NUL:
             return y
         if y == NUL:
             return x
-        return Grammar(self, ALT, other)
+        return Grammar(x, ALT, y)
 
     def __repr__(self):
         return 'Token(%r)' % self.token
@@ -53,6 +53,7 @@ class Token(object):
     def graph(self, graphed, rank):
         gid = graph_id(self)
         if gid in graphed:
+            graphed[gid] = min(rank, graphed[gid])
             return
         print
         print '  %s [label="%s" shape=box]' % (gid, str(self.token))
@@ -68,60 +69,58 @@ ALT = 'ALT'
 
 class Grammar(object):
     def __init__(self, lgrammar, operation=None, rgrammar=None):
-        self.lgrammar = lgrammar.get_token() or lgrammar
+        self.lgrammar = lgrammar.get_value()
         self.operation = operation
-        self.rgrammar = operation and rgrammar.get_token() or rgrammar
+        self.rgrammar = operation and rgrammar.get_value()
         self._derivations = {}
         self._nullable = None
 
-    def get_token(self):
-        if self.operation is None and isinstance(self.lgrammar, Token):
+    def get_value(self):
+        if self.operation is None:
             return self.lgrammar
-        return None
-
-    def _fold(self):
-        assert self.operation is CAT
-        l = self.lgrammar
-        r = self.rgrammar
-        seen = [self]
-        while isinstance(l, Grammar):
-            if l.operation is None:
-                l = l.lgrammar
-                self.lgrammar = l
-                continue
-            if l.operation is not CAT:
-                break
-            if l in seen:
-                # Infinite left recursion, can not match anything
-                print ' .==.'
-                self.lgrammar = NUL
-                self.operation = None
-                self.rgrammar = None
-                return
-            seen.append(l)
-            l, r = l.lgrammar, Grammar(l.rgrammar, CAT, r)
-        self.lgrammar = l
-        self.rgrammar = r
+        return self
 
     def __and__(self, other):
-        x = self.get_token() or self
-        y = other.get_token() or other
+        x = self.get_value()
+        y = other.get_value()
         if x == NUL or y == NUL:
             return NUL
         if x == EPS:
             return y
         if y == EPS:
             return x
-        res = Grammar(x, CAT, y)
-        res._fold()
-        r = res.rgrammar
-        if isinstance(r, Grammar) and r.operation is CAT:
-            r._fold()
-        return res
+        if x is y:
+            return x
+        result = Grammar(x, CAT, y)
+        result._lfold_cats()
+        return result
+
+    def _lfold_cats(self):
+        assert self.operation is CAT
+        l = self.lgrammar
+        r = self.rgrammar
+        seen = [self]
+        while isinstance(l, Grammar):
+            if l.operation is None:
+                parent = seen[-1]
+                seen.append(l)
+                parent.lgrammar = l = l.lgrammar
+                continue
+            elif l.operation is CAT:
+                if l in seen:
+                    # Infinite left recursion, can not match anything
+                    self.setvalues(NUL, None, None)
+                    return
+                seen.append(l)
+                l, r = l.lgrammar, l.rgrammar & r
+            else:
+                break
+        self.lgrammar = l
+        self.rgrammar = r
 
     def __or__(self, other):
-        x = self.get_token() or self
-        y = other.get_token() or other
+        x = self.get_value()
+        y = other.get_value()
         if x == NUL:
             return y
         if y == NUL:
@@ -133,71 +132,111 @@ class Grammar(object):
     @property
     def nullable(self):
         if self._nullable is None:
-            self._nullable = self._is_nullable()
+            self._nullable = ''
+            _nullable = self._is_nullable()
+            if _nullable == '':
+                raise Exception('Doesn\'t work')
+            self._nullable = _nullable
         return self._nullable
 
+    def peek_nullable(self):
+        if self._nullable is not None:
+            return self._nullable
+        _nullable = self._is_nullable()
+        if isinstance(_nullable, bool):
+            self._nullable = _nullable
+        return _nullable
+
     def _is_nullable(self):
-        self._nullable = False  # Initail value to break recursions
         l = self.lgrammar
         r = self.rgrammar
         if self.operation is None:
-            return self.lgrammar.nullable
-        if self.operation is CAT:
-            return l.nullable and r.nullable
-        if self.operation is ALT:
-            return l.nullable or r.nullable
-        assert False, "Bad! %s" % self.operation
+            return l.nullable
+        elif self.operation is CAT:
+            ln = l.nullable if isinstance(l, Token) else l.peek_nullable()
+            rn = r.nullable if isinstance(r, Token) else r.peek_nullable()
+            if ln is False or rn is False:
+                return False
+            return ln and rn
+        elif self.operation is ALT:
+            ln = l.nullable if isinstance(l, Token) else l.peek_nullable()
+            rn = r.nullable if isinstance(r, Token) else r.peek_nullable()
+            if ln is True or rn is True:
+                return True
+            if not isinstance(ln, bool):
+                return ln
+            if not isinstance(rn, bool):
+                return rn
+            return ln or rn
+        else:
+            assert False, "Bad! %s" % self.operation
+
+    def singles_fold(self):
+        for g in self:
+            if not isinstance(g, Grammar):
+                continue
+            while True:
+                l = g.lgrammar
+                if not (isinstance(l, Grammar) and l.operation is None):
+                    break
+                g.lgrammar = l.lgrammar
+            if self.operation is not None:
+                while True:
+                    r = g.rgrammar
+                    if not (isinstance(r, Grammar) and r.operation is None):
+                        break
+                    g.rgrammar = r.rgrammar
+            elif isinstance(self.lgrammar, Grammar):
+                l = self.lgrammar
+                self.lgrammar = l.lgrammar
+                self.operation = l.operation
+                self.rgrammar = l.rgrammar
 
     def derive(self, token):
         try:
             return self._derivations[token]
         except KeyError:
             pass
-        cached = self._derivations[token] = Grammar(LAZY_TOKEN, ALT, EPS)
-        res = self._derive(token)
-        assert res is not cached
-        if not isinstance(res, Grammar) or res.is_lazy():
-            cached.lgrammar = res
-            cached.operation = None
-            cached.rgrammar = None
-            if isinstance(res, Grammar) and res.is_lazy():
-                print ' --x--', graph_id(res), graph_id(cached)
-        else:
-            cached.lgrammar = res.lgrammar
-            cached.operation = res.operation
-            cached.rgrammar = res.rgrammar
-            if cached.operation is CAT:
-                cached._fold()
-        return cached
+        temp = self._derivations[token] = Grammar.create_lazy()
+        result = self._derive(token)
+        assert result is not temp
+        temp.setvalues(result, None, None)
+        self._derivations[token] = result
+        return result
 
     def _derive(self, token):
         if self.lgrammar == LAZY_TOKEN:
             raise Exception('Fake!')
-        l = self.lgrammar
-        r = self.rgrammar
-        res = None
+        l = self.lgrammar.get_value()
+        r = self.rgrammar and self.rgrammar.get_value()
+        self.lgrammar = l
+        self.rgrammar = r
+        result = None
         if self.operation is None:
-            res = l.derive(token)
+            result = l.derive(token)
         elif self.operation is CAT:
-            d1 = l.derive(token) & r
+            d_l = l.derive(token)
             if not l.nullable:
-                res = d1
+                result = d_l & r
             else:
-                res = d1 | r.derive(token)
+                d_r = r.derive(token)
+                result = (d_l & r) | d_r
         elif self.operation is ALT:
-            d1 = l.derive(token)
-            d2 = r.derive(token)
-            res = d1 | d2
+            d_l = l.derive(token)
+            d_r = r.derive(token)
+            result = d_l | d_r
         else:
             assert False, "Bad! %s" % self.operation
-        assert res is not None
-        return res
+        return result
 
     def match_(self, tokens):
         l = self
         for token in tokens:
             l = l.derive(token)
-            if l.get_token() == NUL:
+            global FOLD_SINGLES
+            if FOLD_SINGLES:
+                l.singles_fold()
+            if l.get_value() == NUL:
                 return False
         return l.nullable
 
@@ -213,6 +252,7 @@ class Grammar(object):
             print 'digraph derpy {'
             print 'graph [layout=dot rankdir=TB ordering=out]'
         elif gid in graphed:
+            graphed[gid] = min(rank, graphed[gid])
             return
         l, r = self.lgrammar, self.rgrammar
         lid, rid = graph_id(l), graph_id(r)
@@ -233,7 +273,6 @@ class Grammar(object):
             for rank in sorted(ranks.keys()[1:]):
                 print '  {rank=same; %s}' % ' '.join(ranks[rank])
             print '}'
-        return graphed
 
     def __iter__(self):
         return self._iterate()
@@ -272,25 +311,37 @@ class Grammar(object):
             raise Exception("Fake lazy!")
         return False
 
-    def any_lazy(self, checked=None):
+    def setvalues(self, l, o, r):
+        self.lgrammar = l
+        self.operation = o
+        self.rgrammar = r
+
+    def copyfrom(self, other):
+        self.lgrammar = other.lgrammar
+        self.operation = other.operation
+        self.rgrammar = other.rgrammar
+
+    def any_lazy(self):
         for i in self:
             if isinstance(i, Grammar) and i.is_lazy():
                 return True
         return False
 
-    def derive_many(self, tokens):
+    def derive_raw(self, raw_tokens):
         d = self
-        for token in tokens:
+        for token in map(Token, raw_tokens):
             d = d.derive(token)
+            global FOLD_SINGLES
+            if FOLD_SINGLES:
+                d.singles_fold()
             if d is NUL:
                 break
         return d
 
     def starplus(self):
-        eps = Grammar(EPS)
-        plus = Grammar(eps, CAT, self)
-        star = Grammar(eps, ALT, plus)
-        plus.lgrammar = star
+        plus = Grammar(self, CAT, EPS)
+        star = Grammar(EPS, ALT, plus)
+        plus.rgrammar = star
         return (star, plus)
 
     def star(self):
@@ -300,10 +351,43 @@ class Grammar(object):
         return self.starplus()[1]
 
 
+class SymbolDict(dict):
+    def __init__(self, language):
+        self.language = language
+        self.gdict = defaultdict(Grammar.create_lazy)
+
+    def __getitem__(self, key):
+        return self.gdict[key]
+
+    def __setitem__(self, key, value):
+        g = self.gdict[key]
+        if not g.is_lazy():
+            print 'WARNING: assigning to non-lazy symbol'
+        if isinstance(value, Grammar):
+            g.lgrammar = value.lgrammar
+            g.operation = value.operation
+            g.rgrammar = value.rgrammar
+        else:
+            g.lgrammar = value
+            g.operation = None
+            g.rgrammar = None
+
+
+class Language(object):
+
+    def __init__(self):
+        self.NT = SymbolDict(self)
+        self._tokens = {}
+
+    def T(self, raw_token):
+        return self._tokens.setdefault(raw_token, Token(raw_token))
+
+
 def graph_id(obj):
     ID = str(abs(id(obj)))
     c = chr(ord(ID[0]) + ord('A') - ord('0'))
     return c + ID[1:]
+
 
 chr_range = lambda a, b: ''.join(map(chr, range(ord(a), ord(b)+1)))
 char = lambda c: Grammar(Token(c))
@@ -312,19 +396,52 @@ alpha = anychar(chr_range('A', 'Z') + chr_range('a', 'z'))
 num = anychar('0123456789')
 word = alpha & (alpha | num).star()
 
-a = char('a')
-S = (a & char('(') & a & char(')')) | Grammar(EPS)
-#S.lgrammar.lgrammar.lgrammar.lgrammar = S
-#S.lgrammar.lgrammar.rgrammar = S
-S.lgrammar.rgrammar.rgrammar.lgrammar = S
-S.lgrammar.lgrammar = S
+a = Token('a')
+b = Token('b')
 
-s = (a & char('+') & a) | char('1')
-s.lgrammar.rgrammar.rgrammar = s
-s.lgrammar.lgrammar = s
-#s.lgrammar.lgrammar.lgrammar = s
-#s.lgrammar.rgrammar = s
-"""
-S = (((S o) S) c) | e
-dS = (dS
-"""
+S = Grammar.create_lazy()
+S.copyfrom((S & char('(') & S & char(')')) | EPS)
+
+s = Grammar.create_lazy()
+s.copyfrom(s & char('+') & s | char('1'))
+
+top = Grammar.create_lazy()
+bot = top | EPS
+x = a & bot
+y = bot & b
+top.copyfrom(x & y)
+
+lisp = Grammar.create_lazy()
+lispitem = Grammar.create_lazy()
+lisp.copyfrom((char('(') & lispitem & char(')')) | char('s'))
+lispitem.copyfrom((lispitem & lisp) | EPS)
+FOLD_SINGLES = False
+
+
+def test():
+    assert s.match('') == False
+    assert s.match('1+1') == True
+    assert s.match('1') == True
+    assert s.match('1+' * 10 + '+') == False
+
+    assert S.match('') == True
+    assert S.match('(((()())))') == True
+    assert S.match('()()()((') == False
+
+    assert lisp.match('((s(s)))') == True
+    assert lisp.match('s') == True
+    assert lisp.match('()') == True
+
+
+def prof(n=10, best=3):
+    import time
+    times = []
+    for _ in range(n):
+        for i in (set(s) | {s}):
+            if isinstance(i, Grammar):
+                i._derivations.clear()
+        tstart = time.time()
+        s.match('1' + '+1' * 150)
+        tend = time.time()
+        times.append(tend-tstart)
+    return sum(sorted(times)[:best])/min(n, best)
